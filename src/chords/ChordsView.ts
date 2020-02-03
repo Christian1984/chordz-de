d8572fb3bf30ed0e19062625ca70de58045dffeb
Chris <chris.hoffmann84@gmail.com>
2020-02-03 21:56:10 +0100

    added html elements as view components

--- src/chords/ChordsView.ts
+++ src/chords/ChordsView.ts
@@ -1,7 +1,25 @@
 import { IChordsView } from "./IChordsView.js";
 import { ChordsGame } from "./ChordsGame.js";
+import { Chord } from "./ChordsEnums.js";
 
 export class ChordsView implements IChordsView {
+    private chordDisplay: HTMLElement;
+    private reversalDisplay: HTMLElement;
+    private handDisplay: HTMLElement;
+    private timerDisplay: HTMLElement;
+
+    constructor(
+        chordDisplay: HTMLElement,
+        reversalDisplay: HTMLElement,
+        handDisplay: HTMLElement,
+        timerDisplay: HTMLElement
+    ) {
+        this.chordDisplay = chordDisplay;
+        this.reversalDisplay = reversalDisplay;
+        this.handDisplay = handDisplay;
+        this.timerDisplay = timerDisplay;
+    }
+
     public update(game: ChordsGame) {
         let puzzle = game.getPuzzle();
 
@@ -13,6 +31,45 @@ export class ChordsView implements IChordsView {
                 ", LeftHand;", puzzle.getLeftHand(),
                 ", Seconds;", puzzle.getSeconds()
             );
+
+            this.chordDisplay.textContent = this.chordToString(puzzle.getChord());
+            this.reversalDisplay.textContent = this.reversalToString(puzzle.getReversal());
+            this.handDisplay.textContent = this.handToString(puzzle.getLeftHand());
+            this.timerDisplay.textContent = this.secondsToString(puzzle.getSeconds());
         }
+    }
+
+    private chordToString(chord: Chord): string {
+        switch (chord) {
+            case Chord.C:
+                return "C";
+            case Chord.F:
+                return "F";
+            case Chord.G:
+                return "G";
+        }
+    }
+
+    private reversalToString(reversal: number): string {
+        if (reversal < 1) {
+            return "Grundstellung";
+        }
+        else {
+            return reversal + ". Umkehrung"
+        }
+    }
+
+    private handToString(leftHand: boolean): string {
+        return leftHand ? "Linke Hand" : "Rechte Hand";
+    }
+
+    private secondsToString(seconds: number) : string {
+        let minuteNumber = Math.floor(seconds / 60);
+        let minuteString = minuteNumber > 0 ? minuteNumber.toString() : "0";
+
+        let secondsNumber = Math.floor(seconds % 60);
+        let secondsString = secondsNumber > 9 ? secondsNumber.toString() : "0" + secondsNumber.toString();
+
+        return minuteString + ":" + secondsString;
     }
 }
--- src/chords/chords.ts
+++ src/chords/chords.ts
@@ -2,9 +2,21 @@ import { ChordsGame } from "./ChordsGame.js";
 import { ChordsView } from "./ChordsView.js";
 
 (function() {
-    let view = new ChordsView();
-    let game = new ChordsGame();
-    game.addView(view);
+    let chordsDisplay = document.getElementById("chord-name");
+    let reversalDisplay = document.getElementById("chord-reversal");
+    let handDisplay = document.getElementById("chord-hand");
+    let timerDisplay = document.getElementById("chord-timer");
+
+    if (
+        chordsDisplay &&
+        reversalDisplay &&
+        handDisplay &&
+        timerDisplay
+    ) {
+        let view = new ChordsView(chordsDisplay, reversalDisplay, handDisplay, timerDisplay);
+        let game = new ChordsGame();
+        game.addView(view);
+    }
 
     console.log("done!");
 })();
